7a7ade2253472ae5eb86d16e6d1b166473a4adc0
LauriA1 <t5aula00@students.oamk.fi>
2016-11-09 10:49:38 +0200

    2

--- Sigma/Main/Nopeus/sikma_lcd/sikma_lcd.ino
+++ Sigma/Main/Nopeus/sikma_lcd/sikma_lcd.ino
@@ -9,18 +9,20 @@
 TFT TFTscreen = TFT(cs, dc, rst);
 
 // variables for the position of the ball and paddle
-char nopeus;
-int kello = 0;
-unsigned long time;
+
+
+
 const int buttonPin = 12;
 int buttonState = 0;
 
 int sensorPin = 2;
 float start, tk=22;
-float elapsed;
+float elapsed, time;
 float revs;
 float revolution;
 char sensorPrintout[6];
+String oldsensor;
+
 void setup() {
 
   pinMode(buttonPin, INPUT);
@@ -46,18 +48,19 @@ void setup() {
 void RPM()
 {
   int sensorValue = digitalRead(sensorPin);
-  float revs = 60000/elapsed;
-  float revolution = elapsed/1000;
-  float kmh = ((tk*2.54*3.1459)*revs*60/100000);
   elapsed=millis()-start;
+  float revs = 60000/elapsed;
+  float kmh = ((tk*2.54*3.1459)*revs*60/100000);
   start=millis();
-
+   String sensorVal = String(kmh);
+   sensorVal.toCharArray(sensorPrintout, 6);
 /*  Serial.print(elapsed);
   Serial.print(" mS ");
   Serial.print(revolution);
   Serial.print("SEC ");
   Serial.print(revs);
   Serial.print("RPM ");*/  
+  /*
   TFTscreen.setTextSize(4);
   TFTscreen.stroke(0, 0, 0);
   TFTscreen.text(sensorPrintout, 0, 20);
@@ -70,14 +73,29 @@ void RPM()
  Serial.print(sensorPrintout[2]);
  Serial.print(sensorPrintout[3]);
  Serial.print(sensorPrintout[4]);
- Serial.println(sensorPrintout[5]);
- Serial.println(kmh);
-   delay(250);
+ Serial.println(sensorPrintout[5]);*/
 
 }
 
 void loop() {
-  
+  TFTscreen.setTextSize(4);
+
+ //  String sensorVal = String(kmh);
+   //sensorVal.toCharArray(sensorPrintout, 6);
+   TFTscreen.stroke(255, 255, 255);
+   TFTscreen.text(sensorPrintout, 0, 20);
+   oldsensor = sensorPrintout;
+ Serial.print(sensorPrintout[0]);
+ Serial.print(sensorPrintout[1]);
+ Serial.print(sensorPrintout[2]);
+ Serial.print(sensorPrintout[3]);
+ Serial.print(sensorPrintout[4]);
+ Serial.println(sensorPrintout[5]);
+ delay(250);
+  TFTscreen.stroke(0, 0, 0);
+ TFTscreen.text(sensorPrintout, 0, 20);
+
+  /*
   buttonState = digitalRead(buttonPin);
 
   TFTscreen.setTextSize(4);
@@ -86,10 +104,8 @@ void loop() {
   TFTscreen.text(nopeus, 0, 20);
   TFTscreen.setTextSize(4);
   TFTscreen.stroke(1024, 1000, 1024);
-  TFTscreen.text(" KM/H\n", 30, 50);
+  TFTscreen.text(" KM/H\n", 30, 50);*/
 
-  delay(250);
-  TFTscreen.stroke(0, 0, 0);
-  TFTscreen.text(kello, 60, 100);
+
 }
 
