acdcf9ddbbca7a5d7a232e541f78be743668bd5d
LauriA1 <t5aula00@students.oamk.fi>
2016-11-14 14:32:19 +0200

    mph

--- Sigma/Main/Nopeus/sikma_lcd/sikma_lcd.ino
+++ Sigma/Main/Nopeus/sikma_lcd/sikma_lcd.ino
@@ -7,8 +7,8 @@
 
 TFT TFTscreen = TFT(cs, dc, rst);
 unsigned long t,s,m,h,seconds,secondsoff,oldseconds;
-int sensorPin = 5, v=1,matkaold,del=0;
-float start, tk=22, kierrokset = 0, matka = 0, revs, elapsed, time,matkat,matkar,matkaoff;
+int sensorPin = 5, v=1,del=0,mph = 0;
+float start, tk=22, kierrokset = 0, matka = 0, revs, elapsed, time,matkat,matkar,matkaoff,matkaold;
 char oldsensor[6], Matka[6], sensorPrintout[6], secc[4], mnc[4], hrc[4],MatkaT[6];
 String oldVal,sensorVal,matkaVal,sec,minn,hou,matkaT;
 char minuutit[10], sekunnit[10], tunnit[10];
@@ -24,11 +24,14 @@ void setup() {
 
   // write the static text to the screen
   // set the font color to white
-  TFTscreen.stroke(1000, 950, 950);
+  TFTscreen.stroke(1000, 200, 200);
   // set the font size
   TFTscreen.setTextSize(2);
   // write the text to the top left corner of the screen
   TFTscreen.text("SIKMA ", 0, 0);
+  TFTscreen.line(0, 55, 160, 55);
+
+  
   // ste the font size very large for the loop
   // TFTscreen.setTextSize(4);
   Serial.begin(9600);
@@ -45,9 +48,15 @@ void RPM()
   start=millis();
     float revs = 60000/elapsed;
     float kmh = ((tk*2.54*3.1459)*revs*60/100000);
-    sensorVal = String(kmh);
+    
   kierrokset += 1;
   matka = kierrokset * (tk*2.54*3.1459)/100000;
+  if(mph==1)
+  {
+    kmh = kmh * 0.621371;
+    matka = matka * 0.621371;
+  }
+  sensorVal = String(kmh);
 }
 /*
 void lisa()
@@ -64,20 +73,22 @@ void reset()
 void loop() {
 	
 	
-  int abc = digitalRead(5);
+  matkar=matka-matkaoff;
   del=0;
-  while(abc == HIGH)
+ int f = 0;
+  while(digitalRead(5) == HIGH)
   {
     del=del+1;
-	delay(100);
-	abc = digitalRead(5);
+	delay(1);
+ f=1;
   }
-  if(del<5){
+  if(del<500 && f==1){
 	  v+=1;
 	if(v>4){
 	v=1;
   }
-  else if(del>5){
+  }
+  if(del>500 && f==1){
 	secondsoff = millis() / 1000;
 	matkaoff = matka;
   }
@@ -107,27 +118,55 @@ void loop() {
  Serial.print(sensorPrintout[3]);
  Serial.print(sensorPrintout[4]);
  Serial.println(sensorPrintout[5]);
- 
-	
+ if(mph == 1)
+ {
+	 TFTscreen.setTextSize(1);
+    TFTscreen.stroke(1000, 1000, 1000);
+  TFTscreen.text("MP/h", 130, 41);
+ }
+ else
+ {
+  TFTscreen.setTextSize(1);
+    TFTscreen.stroke(1000, 1000, 1000);
+  TFTscreen.text("KM/h", 130, 41);
+ }
   }
 
 	switch (v){
   
-  if(matka!=matkaold)
+  
 	case 1:
-	TFTscreen.setTextSize(2);
+ if(matkar!=matkaold)
+ {
+
+	
 	TFTscreen.stroke(0, 0, 0);
+  TFTscreen.setTextSize(1);
+  TFTscreen.text("TOTAL", 60, 65);
+
+  TFTscreen.setTextSize(2);
 	TFTscreen.text(Matka, 0, 60);
   TFTscreen.text(tunnit, 0, 60);
   TFTscreen.text(MatkaT, 0, 60);
-	matkar=matka-matkaoff;
-	matkaold=matka;
+	//matkar=matka-matkaoff;
+	matkaold=matkar;
 	matkaVal = String (matkar);
     matkaVal.toCharArray(Matka, 6);
     TFTscreen.stroke(255, 255, 255);
+
+    TFTscreen.setTextSize(2);
     TFTscreen.text(Matka, 0, 60);
 
-	
+    if(mph == 1)
+    {
+      TFTscreen.setTextSize(1);
+      TFTscreen.text("miles TRIP", 60, 65);
+    }
+    else
+    {
+      TFTscreen.setTextSize(1);
+      TFTscreen.text("km TRIP", 60, 65);
+    }
 	
 	Serial.print(Matka[0]);
 	Serial.print(Matka[1]);
@@ -135,9 +174,9 @@ void loop() {
 	Serial.print(Matka[3]);
 	Serial.println(Matka[4]);
  Serial.println("CASE1");
- delay(1000);
+ }
 	break;
-	
+ 
 	case 2:
 	
 	if(seconds!=oldseconds){
@@ -153,8 +192,11 @@ void loop() {
     m = t % 60;
     t = (t - m)/60;
     h = t;
-	
+TFTscreen.setTextSize(1);
   TFTscreen.stroke(0, 0, 0);
+ TFTscreen.text("miles TRIP", 60, 65);
+ TFTscreen.text("km TRIP", 60, 65);
+ TFTscreen.setTextSize(2);
  TFTscreen.text(Matka, 0, 60);
  TFTscreen.text(tunnit, 0, 60);
  TFTscreen.text(MatkaT, 0, 60);
@@ -181,6 +223,8 @@ void loop() {
 	break;
 	
 	case 3:
+  TFTscreen.setTextSize(1);
+  TFTscreen.text("TOTAL", 60, 65);
 	TFTscreen.setTextSize(2);
 	TFTscreen.stroke(0, 0, 0);
 	TFTscreen.text(Matka, 0, 60);
@@ -206,6 +250,6 @@ void loop() {
 	break;
 }
   }
-}
+
 
 
