d0a48be90739dc69f95f908fa60691f5a7c6e558
LauriA1 <t5aula00@students.oamk.fi>
2016-12-08 09:38:44 +0200

    docu

--- Sigma/Main/Nopeus/sikma_lcd/sikma_lcd.ino
+++ Sigma/Main/Nopeus/sikma_lcd/sikma_lcd.ino
@@ -1,3 +1,27 @@
+/*
+*		SIKMA
+
+*The circuit
+*	Button	
+*		BUTTON = pin 5, pin 3
+*
+*	Hall Effect Switch ICs C1024 
+*		Schmitt trigger and open-collector output. 
+*		Sensor Vout pull-up resistor to Vcc.
+*		Vout 	= pin 2
+*
+*	Arduino Tft Screen
+*		Screen GND pin switch IRF640N MOSFET.
+*		SCK 	= pin 13
+*		MOSI 	= pin 11
+*		CS 		= pin 10
+*		DC 		= pin 9
+*		RST 	= pin 8
+*
+*	Adafruit DS1307 RTC
+*		SDA 	= pin A4
+*		SCL		= pin A5
+*/
 #include <TFT.h>  //Arduino LCD library
 #include <SPI.h>
 #include <EEPROM.h>
